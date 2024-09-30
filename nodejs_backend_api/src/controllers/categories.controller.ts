bd81dd79ba17f063e1403d882078bd764d9efc98
CristianToan <toanpham9412@gmail.com>
2024-09-30 19:07:09 +0700

    Hung_code

--- nodejs_backend_api/src/controllers/categories.controller.ts
+++ nodejs_backend_api/src/controllers/categories.controller.ts
@@ -1,18 +1,59 @@
-import {Request, Response, NextFunction} from 'express'
+import { Request, Response, NextFunction } from 'express'
 import categoriesService from '../services/categories.service'
+import { sendJsonSuccess } from "../helpers/responseHandler"
 
-const findAll = async (req: Request, res: Response, next: NextFunction) => {
+// 1.Get all Categories
+const findAllCategory = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const categories = await categoriesService.findAll()
-        res.status(200).json({
-            data: categories
-        })
+        const categories = await categoriesService.findAllCategory(req.query)
+        sendJsonSuccess(res, "success")(categories)
     } catch (error) {
         next(error)
     }
-    
+
+}
+// 2. Find Category By Id
+const findCategoryById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id } = req.params
+        const category = await categoriesService.findCategoryById(id)
+        return sendJsonSuccess(res, "success")(category)
+    } catch (error) {
+        next(error)
+    }
+}
+// 3. Create Category 
+const createCategory = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const payload = req.body
+        const category = await categoriesService.createRecord(payload)
+        sendJsonSuccess(res, 'success', 201)(category)
+    } catch (error) {
+        next(error)
+    }
+}
+// 4. update Category
+const updateCategoryById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id } = req.params
+        const payload = req.body
+        const category = await categoriesService.updateCategory(id, payload)
+        sendJsonSuccess(res, "success")(category)
+    } catch (error) {
+        next(error)
+    }
+}
+// 5. delete Category
+const deleteCategory = async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params
+    const category = await categoriesService.deleteCategory(id)
+    sendJsonSuccess(res, "success")(category)
 }
 
 export default {
-    findAll
+    findAllCategory,
+    findCategoryById,
+    createCategory,
+    updateCategoryById,
+    deleteCategory
 }
--- nodejs_backend_api/src/routes/v1/categories.route.ts
+++ nodejs_backend_api/src/routes/v1/categories.route.ts
@@ -3,7 +3,18 @@ import categoriesController from "../../controllers/categories.controller";
 const router = express.Router();
 
 //1. Get All Categories
-router.get('', categoriesController.findAll)
+router.get('', categoriesController.findAllCategory)
 
+// 2.Find Category By Id
+router.get('/:id', categoriesController.findCategoryById)
+
+// 3.Create Category
+router.post('', categoriesController.createCategory)
+
+// // 4.update Category
+router.put('/:id', categoriesController.updateCategoryById)
+
+// // 5.delete Category
+router.delete('/:id', categoriesController.deleteCategory)
 
 export default router
